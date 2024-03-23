d976bd486ebdc6426f2b04f4b381ffb18c080157
justyinaa <142683103+justyinaa@users.noreply.github.com>
2024-03-23 21:07:30 +0100

    Commit

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p id="footer">&copy; 2024 Justina Ominisan</p>
+      
+    </div>
+  );
+}
+
+export default Footer;
